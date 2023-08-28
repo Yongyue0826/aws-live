6cba4fbb0cc0b8baaf3995fc91c341068801da09
Yong Yue <109788606+Yongyue0826@users.noreply.github.com>
2023-08-28 11:50:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanyongyue-employee"
+customregion = "us-east-1"
